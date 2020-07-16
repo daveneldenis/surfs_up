74428a44edf5b6b6772300fd169d18c28555d692
Davenel Denis <daveneldenis@gmail.com>
2020-07-16 18:01:48 -0400

    Completed Flask app

--- app.py
+++ app.py
@@ -53,19 +53,17 @@ def temp_monthly():
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
-        sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)] 
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]           
 
-        if not end: 
-            results = session.query(*sel).\
-        filter(Measurement.date <= start).all()
+    if not end: 
+        results = session.query(*sel).filter(Measurement.date <= start).all()
         temps = list(np.ravel(results))
-        return jsonify(temps)
-
-    results = session.query(*sel).\
-        filter(Measurement.date >= start).\
-        filter(Measurement.date <= end).all()
-    temps = list(np.ravel(results))
-    return jsonify(temps=temps)      
+    else:
+        results = session.query(*sel).filter(Measurement.date >= start)
+        results = results.filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+ 
 
 
 
